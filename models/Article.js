eb5e0940c0960d5973972f707e4a410f8a9754cb
Bindhu <biji_100@yahoo.com>
2019-04-25 17:46:15 -0600

    update changes

--- models/Article.js
+++ models/Article.js
@@ -1,44 +1,89 @@
-var mongoose = require("mongoose");
 
-// Save a reference to the Schema constructor
+// Require mongoose
+var mongoose = require("mongoose");
+var Note = require("./Note");
+// Create Schema class
 var Schema = mongoose.Schema;
 
-// Using the Schema constructor, create a new UserSchema object
-// This is similar to a Sequelize model
+// Create article schema
 var ArticleSchema = new Schema({
-  // `Headline` is required and of type String
-  headline: {
+  title: {
     type: String,
     required: true
   },
-
-// `Summary` is required and of type String
-summary: {
-  type: String
-},
-
-// `url` is required and of type String
-
-url: {
-  type: String
-}, 
-
-isSaved: {
-  type: Boolean,
-  default: false
-},
-
-  // `note` is an object that stores a Note id
-  // The ref property links the ObjectId to the Note model
-  // This allows us to populate the Article with an associated Note
-  note: {
-    type: Schema.Types.ObjectId,
-    ref: "Note"
-  }
+  summary: {
+    type: String,
+    required: true
+  },
+  link: {
+    type: String,
+    required: true
+  },
+  saved: {
+    type: Boolean,
+    default: false
+  },
+  notes: [{
+     type: Schema.Types.ObjectId,
+     ref: "Note"
+  }]
 });
 
-// This creates our model from the above schema, using mongoose's model method
+// Create the Article model with the ArticleSchema
 var Article = mongoose.model("Article", ArticleSchema);
 
-// Export the Article model
+// Export the model
 module.exports = Article;
+
+
+
+
+
+
+// var mongoose = require("mongoose");
+
+// // Save a reference to the Schema constructor
+// var Schema = mongoose.Schema;
+
+// // Using the Schema constructor, create a new UserSchema object
+// // This is similar to a Sequelize model
+// var ArticleSchema = new Schema({
+//   // `Headline` is required and of type String
+//   headline: {
+//     type: String,
+//     required: true
+//   },
+
+// // `Summary` is required and of type String
+// summary: {
+//   type: String
+// },
+
+// // `url` is required and of type String
+
+// url: {
+//   type: String
+// }, 
+
+// isSaved: {
+//   type: Boolean,
+//   default: false
+// },
+
+//   // `note` is an object that stores a Note id
+//   // The ref property links the ObjectId to the Note model
+//   // This allows us to populate the Article with an associated Note
+//   note: {
+//     type: Schema.Types.ObjectId,
+//     ref: "Note"
+//   }
+// });
+
+// // This creates our model from the above schema, using mongoose's model method
+// var Article = mongoose.model("Article", ArticleSchema);
+
+// // Export the Article model
+// module.exports = Article;
+
+// 888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888
+
---
+++ routes/apiroutes.js
@@ -0,0 +1,13 @@
+// var db = require("../models");
+// var axios = require("axios");
+// var cheerio = require("cheerio");
+
+// module.exports = function(app){
+//   // scrape route
+//   // display
+//   // save
+//   // delete
+//   app.get("/saved/:id", function (req, res){
+//     db.Article.updateOne({ _id: req.params.id },{isSaved: true}).then(function (data){});
+//   })
+// }
