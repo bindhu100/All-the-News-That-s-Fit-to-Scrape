fcac867ae036d4c03563134b65a0194711492086
Bindhu <biji_100@yahoo.com>
2019-05-18 09:40:59 -0600

    update scrape link

--- scrape/scrape.js
+++ scrape/scrape.js
@@ -1,26 +1,30 @@
 // Scraping tools
 var request = require("request");
+var axios = require('axios');
 var cheerio = require("cheerio");
 var Article = require("../models/Article");
 
 var scrape = function () {
   return new Promise((resolve, reject) => {
     // First, we grab the body of the html with request
-    request("https://www.cnn.com/specials/health/live-longer", function (error, response, html) {
+   axios.get("https://www.cnn.com/specials/health/live-longer")
+    .then(function (response) {
       // Then, we load that into cheerio and save it to $ for a shorthand selector
-      console.log(response);
-      var $ = cheerio.load(html);
+      console.log(Object.keys(response.data));
+      var $ = cheerio.load(response.data);
       // Now, we grab every h2 within an article tag, and do the following:
-      $("span.cd__headline-text").each(function (i, element) {
+      $("span.cd__headline-text").each(function (element) {
 
+        console.log({ this: $(this)} );
 
         // Add the title and summary of every link, and save them as properties of the result object
         const title = $(this).text();
         const link = $(this).parent("a").attr("href");
+        
 
         const result = {
           title,
-          link
+          link: `https://cnn.com${link}`
         }
 
         console.log(result);
@@ -45,7 +49,10 @@ var scrape = function () {
         resolve();
 
       });
-    });
+    })
+    .catch(error => {
+      console.error(error);
+    })
     // Tell the browser that we finished scraping the text
   });
 }
